a558936ffe75d7367c0d1f6f5164c7bc4618ca91
gprakash3 <gyan.p3008@gmail.com>
2023-09-25 23:14:35 +0530

    returing different response on different url

---
+++ response_on_url.js
@@ -0,0 +1,42 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+
+    const url=req.url;
+    if(url==='/home')
+    {
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>');
+        res.write('<head><title>This is my first response page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/about')
+    {
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>');
+        res.write('<head><title>This is my first response page</title></head>');
+        res.write('<body><h1> Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/node')
+    {
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>');
+        res.write('<head><title>This is my first response page</title></head>');
+        res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>This is my first response page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
+})
+
+server.listen(3000);
