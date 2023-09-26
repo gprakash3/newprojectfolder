a40bf4f4ef82f7b1ca6d6193058ded921123549a
gprakash3 <gyan.p3008@gmail.com>
2023-09-26 23:00:50 +0530

    code is cleaned by importing/exporting  from files

---
+++ clean_up_code/route.js
@@ -0,0 +1,53 @@
+
+const fs = require('fs');
+
+var ndata;
+
+const requestHandler = (req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        //writing data from file in body
+        res.write(`<body>${ndata}</body>`);
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    // if address is /message and method is post
+    if (url === '/message' && method === 'POST') {
+        //creating array named body
+        const body = [];
+        //to get  request data as chunk and push it to array named body
+        req.on('data', (chunk) => {
+            // console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            //converting entered data to string
+            const parsedBody = Buffer.concat(body).toString();
+            //To store entered data to message
+            const message = parsedBody.split('=')[1];
+            //To write entered data to message.txt file
+            fs.writeFileSync('message.txt', message);
+    
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            //reading data from file and storing data to variable ndata
+            fs.readFile('./message.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                ndata = data;
+            });
+             return res.end();
+        });
+    }
+    
+}
+
+module.exports = requestHandler;
