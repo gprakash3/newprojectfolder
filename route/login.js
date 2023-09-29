112990f1d86126ca453ee48b25504e7adaeb91fd
gprakash3 <gyan.p3008@gmail.com>
2023-09-29 23:00:15 +0530

    chat application using local storage

---
+++ route/login.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+//create router
+const router=express.Router();
+
+//local storage importing
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
+//storing username to local storage
+router.get('/login' , (req, res, next) => {
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/chat" method="POST">Username:<input type="text" id="username" name="username"><br><button type="submit">Login</button></form>')
+});
+
+module.exports = router;
